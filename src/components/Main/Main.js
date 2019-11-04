d2d782f9df7a7e5e3bce107a80862a8e1ad587d0
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-04 20:58:46 +0100

    outsourced Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './Main.module.css'
 
 class Main extends Component {
     state = {
@@ -6,7 +7,14 @@ class Main extends Component {
     }
     
     render() {
-        return (null)
+        return (
+            <main className={styles.Centered}>
+                <div className={styles.exampleText}>example text</div>
+                <div className={styles.question}>przetłumacz</div>
+                <input type="text" className={styles.answer}></input>
+                <button className={styles.buttonConfirm}>Sprawdź    </button>
+            </main>
+        )
     }
 }
 
