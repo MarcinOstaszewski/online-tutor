f0322d76eafc2a29ffe278df767084c462ceea5f
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-05 22:12:44 +0100

    add new word to list

--- src/assets/wordLists/wordsPost.js
+++ src/assets/wordLists/wordsPost.js
@@ -5,11 +5,47 @@ let words_post = {
     },
     1: {
         q: 'list', 
-        a : 'letter',
+        a: 'letter',
     },
     2: {
         q: 'listonosz', 
         a: 'mailman'
+    },
+    3: {
+        q: 'papier', 
+        a : 'paper',
+    },
+    4: {
+        q: 'znaczek', 
+        a: 'stamp',
+    },
+    5: {
+        q: 'koperta', 
+        a: 'envelope'
+    },
+    6: {
+        q: 'paczka',
+        a: 'parcel',
+    },
+    7: {
+        q: 'kasa',
+        a: 'cashier',
+    },
+    8: {
+        q: 'urzędnik',
+        a: 'clerk'
+    },
+    9: {
+        q: 'skrzynka',
+        a: 'box',
+    },
+    10: {
+        q: 'klient',
+        a: 'client',
+    },
+    11: {
+        q: 'pieczątka',
+        a: 'stamp'
     }
 }
 
