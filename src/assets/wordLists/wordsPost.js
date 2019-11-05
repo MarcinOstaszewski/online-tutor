09e05ef3a94920254f0d919675fc54658e2993a3
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-05 20:36:03 +0100

    add first example word list

---
+++ src/assets/wordLists/wordsPost.js
@@ -0,0 +1,16 @@
+let words_post = {
+    0: {
+        q: 'skrzynka', 
+        a : 'box',
+    },
+    1: {
+        q: 'list', 
+        a : 'letter',
+    },
+    2: {
+        q: 'listonosz', 
+        a: 'mailman'
+    }
+}
+
+export default words_post;
