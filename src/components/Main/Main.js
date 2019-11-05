ef2f366a90c7bef5e7b175b6d0a625bc90c859bd
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-05 22:13:20 +0100

    add starting and randomising question

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,22 +5,28 @@ import words_post from '../../assets/wordLists/wordsPost';
 
 class Main extends Component {
     state = {
+        lastRandom: 0,
         random: 0,
-        answerText: ''
+        question: '> Naciśnij start <',
+        answerText: '',
+        started: false
     }
-
+    
     list = words_post; // later it will fetch lists from DB
 
-    rand = (max) => Math.floor(Math.random() * max);
+    rnd = (max) => {
+        return Math.floor(Math.random() * max);
+    } 
 
     chooseQuestion = () => {
-        console.log(this.list[this.state.random].q)
-
         return this.list[this.state.random].q
     }
 
     checkAnswer = () => {
-        return this.list[this.state.random].a
+        // console.log(this.state.lastRandom);
+        // console.log(this.list[this.state.lastRandom].a);
+        
+        return this.list[this.state.lastRandom].a;
     }
 
     changeAnswerHandler = (event) => {
@@ -30,27 +36,53 @@ class Main extends Component {
         })
     }
 
-    componentDidMount = () => {
+    updateRnd = () => {
+        let rnd = this.state.random;
         this.setState({
-            random: this.rand( Object.keys(this.list).length )
+            lastRandom: rnd,
+            random: this.rnd(  Object.keys(this.list).length  )
         })
-        this.chooseQuestion();
+    }
+
+    updateQuestion = () => {
+        this.updateRnd();
+        console.log(this.state.random);
+        this.setState({
+            question: this.chooseQuestion()
+        })
+    }
+
+    startGame = () => {
+        this.updateQuestion();
+        this.setState({
+            started: true
+        })
+    }
+
+    componentDidMount = () => {
+        this.updateRnd();
+        console.log(this.state.question)
     }
     
     render() {
+        let button;
+        this.state.started ? 
+            button = <button className={[styles.button, styles.btnConfirm].join(' ')}
+                onClick={this.checkAnswer}>Sprawdź</button> :
+            button = <button className={[styles.button, styles.btnStart].join(' ')}
+                onClick={this.startGame}>START</button>
+        ;
+
         return (
             <main className={styles.Centered}>
                 <div className={styles.exampleText}>example text</div>
-                <div className={styles.question}></div>
+                <div className={styles.questionDiv}>{this.state.question}</div>
                 <input type="text" 
                     name="answerText"
-                    className={styles.answer}
+                    className={styles.answerInput}
                     onChange={this.changeAnswerHandler}
-                    value={this.state.answerText}></input>
-                <button className={styles.buttonConfirm}
-                    onClick={this.checkAnswer}>Sprawdź</button>
-                {/* <button className={styles.buttonStart}
-                    onClick={this.checkAnswer}>START</button> */}
+                    value={this.state.answerText} />
+                {button}
             </main>
         )
     }
