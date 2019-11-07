25bbb714ad03218e2f4966627d8a5708986c4a27
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-07 22:47:21 +0100

    create word list for Animals

---
+++ src/assets/wordLists/wordsAnimals.js
@@ -0,0 +1,197 @@
+const wordsAnimals = {
+    0: {
+        q: 'jeż', 
+        a: 'hedgehog'
+    },
+    1: {
+        q: 'lis', 
+        a: 'fox'
+    },
+    2: {
+        q: 'lew', 
+        a: 'lion'
+    },
+    3: {
+        q: 'pantera',
+        a: 'panther'
+    },
+    4: {
+        q: 'żyrafa', 
+        a: 'giraffe'
+    },
+    5: {
+        q: 'słoń', 
+        a: 'elephant'
+    },
+    6: {
+        q: 'wielbłąd',
+        a: 'camel'
+    },
+    7: {
+        q: 'pawian',
+        a: 'baboon'
+    },
+    8: {
+        q: 'goryl',
+        a: 'gorilla'
+    },
+    9: {
+        q: 'nosorożec',
+        a: 'rhinoceros'
+    },
+    10: {
+        q: 'kurczak',
+        a: 'chicken'
+    },
+    11: {
+        q: 'kura',
+        a: 'hen'
+    },
+    12: {
+        q: 'gibon',
+        a: 'gibbon'
+    },
+    13: {
+        q: 'królik', 
+        a: 'rabbit'
+    },
+    14: {
+        q: 'ryba', 
+        a: 'fish'
+    },
+    15: {
+        q: 'niedźwiedź', 
+        a: 'bear'
+    },
+    16: {
+        q: 'krokodyl',
+        a: 'crockodile'
+    },
+    17: {
+        q: 'małpa',
+        a: 'monkey'
+    },
+    18: {
+        q: 'ptak',
+        a: 'bird'
+    },
+    19: {
+        q: 'jeleń',
+        a: 'deer'
+    },
+    20: {
+        q: 'mysz',
+        a: 'mouse'
+    },
+    21: {
+        q: 'żaba',
+        a: 'frog'
+    },
+    22: {
+        q: 'koza',
+        a: 'goat'
+    },
+    23: {
+        q: 'chomik', 
+        a: 'hamster'
+    },
+    24: {
+        q: 'koń', 
+        a: 'horse'
+    },
+    25: {
+        q: 'kangur', 
+        a: 'kangaroo'
+    },
+    26: {
+        q: 'owca',
+        a: 'sheep'
+    },
+    27: {
+        q: 'papuga',
+        a: 'parrot'
+    },
+    28: {
+        q: 'świnia',
+        a: 'pig'
+    },
+    29: {
+        q: 'kucyk',
+        a: 'pony'
+    },
+    30: {
+        q: 'szczur',
+        a: 'rat'
+    },
+    31: {
+        q: 'wiewiórka',
+        a: 'squirrel'
+    },
+    32: {
+        q: 'ropucha',
+        a: 'toad'
+    },
+    33: {
+        q: 'tygrys', 
+        a: 'tiger'
+    },
+    34: {
+        q: 'wieloryb', 
+        a: 'whale'
+    },
+    35: {
+        q: 'wilk', 
+        a: 'woolf'
+    },
+    36: {
+        q: 'jagnię', 
+        a: 'lamb'
+    },
+    37: {
+        q: 'wąż', 
+        a: 'snake'
+    },
+    38: {
+        q: 'jaszczurka',
+        a: 'lizzard'
+    },
+    39: {
+        q: 'orzeł',
+        a: 'eagle'
+    },
+    40: {
+        q: 'jastrząb',
+        a: 'hawk'
+    },
+    41: {
+        q: 'kaczka',
+        a: 'duck'
+    },
+    42: {
+        q: 'bawół',
+        a: 'buffalo'
+    },
+    43: {
+        q: 'niedźwiedź polarny', 
+        a: 'polar bear'
+    },
+    44: {
+        q: 'koala', 
+        a: 'coala'
+    },
+    45: {
+        q: 'dziobak', 
+        a: 'platypus'
+    },
+    46: {
+        q: 'wróbel', 
+        a: 'sparow'
+    },
+    47: {
+        q: 'gołąb', 
+        a: 'pidgeon'
+    }
+}
+
+// export { wordsAnimals };
+export default wordsAnimals;
