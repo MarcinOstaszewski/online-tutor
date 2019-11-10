0374ff24477a1003a53aeab5a859f7eed647d0cf
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-10 23:16:13 +0100

    add sending chosen list name to Main

--- src/containers/MainMenuList/MainMenuList.js
+++ src/containers/MainMenuList/MainMenuList.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react'
 
+
 import styles from './MainMenuList.module.css';
 
 class MainMenuList extends Component {
 
-    render() {
-        let MainListClass = this.props.visible ? (styles.Visible) : (styles.inVisible);
+    listChosen = (e) => {
+        this.props.clickHandler(e);
+        this.props.hideList(e);
+    }
 
-        console.log('List: ' + this.props.visible)
+    render() {
 
         return (
-            <ul className={[styles.MainMenuList, MainListClass].join(' ')} >
-                <li>Animals1</li>
-                <li>Animals2</li>
-                <li>Animals3</li>
-                <li>Post Ofice</li>
+            <ul className={[styles.MainMenuList, this.props.visible ? (styles.Visible) : (styles.inVisible)].join(' ')} >
+                <li onClick={this.listChosen}>Animals1</li>
+                <li onClick={this.listChosen}>Animals2</li>
+                <li onClick={this.listChosen}>Animals3</li>
+                <li onClick={this.listChosen}>Post Office</li>
             </ul>
          );
     }
