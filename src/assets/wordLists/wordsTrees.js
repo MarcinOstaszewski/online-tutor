607c1464fa220b77ff7934af56782168bc54eadf
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-11 23:08:41 +0100

    add wordsTrees list

---
+++ src/assets/wordLists/wordsTrees.js
@@ -0,0 +1,240 @@
+const wordsTrees1 = {
+    "1": {
+        "pol": "brzoza",
+        "eng": "birch-tree",
+        "ger": "Birke",
+        "esp": "abedul"
+    },
+    "2": {
+        "pol": "buk",
+        "eng": "beech",
+        "ger": "Buche",
+        "esp": "haya"
+    },
+    "3": {
+        "pol": "cis",
+        "eng": "yew",
+        "ger": "Eibe",
+        "esp": "tejo"
+    },
+    "4": {
+        "pol": "cyprys",
+        "eng": "cypress",
+        "ger": "Zypresse",
+        "esp": "ciprés"
+    },
+    "5": {
+        "pol": "czeremcha",
+        "eng": "bird cherry",
+        "ger": "Vogelkirsche ",
+        "esp": "cereza de pájaro"
+    },
+    "6": {
+        "pol": "dąb",
+        "eng": "oak",
+        "ger": "Eiche",
+        "esp": "roble"
+    },
+    "7": {
+        "pol": "dereń",
+        "eng": "dogwood",
+        "ger": "Hartriegel",
+        "esp": "cornejo"
+    },
+    "8": {
+        "pol": "głóg",
+        "eng": "hawthorn",
+        "ger": "Hagedorn",
+        "esp": "espino"
+    },
+    "9": {
+        "pol": "grab",
+        "eng": "hornbeam",
+        "ger": "Hainbuche ",
+        "esp": "carpe"
+    },
+    "10": {
+        "pol": "grusza",
+        "eng": "pear",
+        "ger": "Birne",
+        "esp": "pera"
+    },
+    "11": {
+        "pol": "jabłoń",
+        "eng": "apple-tree",
+        "ger": "Apfelbaum ",
+        "esp": "manzano"
+    },
+    "12": {
+        "pol": "jarząb",
+        "eng": "sorb",
+        "ger": "sorb",
+        "esp": "serbal"
+    },
+    "13": {
+        "pol": "jarzębina",
+        "eng": "mountain ash",
+        "ger": "Eberesche",
+        "esp": "serbal"
+    },
+    "14": {
+        "pol": "jesion",
+        "eng": "ash",
+        "ger": "Asche",
+        "esp": "ceniza"
+    },
+    "15": {
+        "pol": "jodła",
+        "eng": "fir",
+        "ger": "Tanne",
+        "esp": "abeto"
+    },
+    "16": {
+        "pol": "klon",
+        "eng": "clone",
+        "ger": "Ahorn",
+        "esp": "arce"
+    },
+    "17": {
+        "pol": "leszczyna",
+        "eng": "hazel",
+        "ger": "Haselnuss",
+        "esp": "avellana"
+    },
+    "18": {
+        "pol": "lilak",
+        "eng": "lilac",
+        "ger": "Flieder",
+        "esp": "lila"
+    },
+    "19": {
+        "pol": "limba",
+        "eng": "pine",
+        "ger": "Kiefer",
+        "esp": "pino"
+    },
+    "20": {
+        "pol": "lipa",
+        "eng": "linden",
+        "ger": "Kalk",
+        "esp": "cal"
+    }
+}
+const wordsTrees2 = {
+    "21": {
+        "pol": "magnolia",
+        "eng": "magnolia",
+        "ger": "Magnolie",
+        "esp": "magnolia"
+    },
+    "22": {
+        "pol": "miłorząb",
+        "eng": "ginkgo",
+        "ger": "ginkgo",
+        "esp": "gingko"
+    },
+    "23": {
+        "pol": "modrzew",
+        "eng": "larch",
+        "ger": "Lärche",
+        "esp": "alerce"
+    },
+    "24": {
+        "pol": "morwa",
+        "eng": "mulberry",
+        "ger": "Maulbeere",
+        "esp": "morera"
+    },
+    "25": {
+        "pol": "olcha",
+        "eng": "alder",
+        "ger": "Erle",
+        "esp": "aliso"
+    },
+    "26": {
+        "pol": "orzech",
+        "eng": "nut",
+        "ger": "Nuss",
+        "esp": "tuerca"
+    },
+    "27": {
+        "pol": "osika",
+        "eng": "aspen",
+        "ger": "Espe",
+        "esp": "álamo temblón"
+    },
+    "28": {
+        "pol": "ostrokrzew",
+        "eng": "holly",
+        "ger": "Stechpalme ",
+        "esp": "acebo"
+    },
+    "29": {
+        "pol": "platan",
+        "eng": "plane-tree",
+        "ger": "Platane",
+        "esp": "plátano"
+    },
+    "30": {
+        "pol": "robinia",
+        "eng": "robinia",
+        "ger": "Robinie",
+        "esp": "robinia"
+    },
+    "31": {
+        "pol": "sosna",
+        "eng": "pine",
+        "ger": "Kiefer",
+        "esp": "pino"
+    },
+    "32": {
+        "pol": "sumak",
+        "eng": "sumach",
+        "ger": "sumach",
+        "esp": "zumaque"
+    },
+    "33": {
+        "pol": "śliwa",
+        "eng": "plum-tree",
+        "ger": "Pflaumenbaum ",
+        "esp": "ciruelo"
+    },
+    "34": {
+        "pol": "świerk",
+        "eng": "spruce",
+        "ger": "Fichte",
+        "esp": "picea"
+    },
+    "35": {
+        "pol": "topola",
+        "eng": "poplar",
+        "ger": "Pappel",
+        "esp": "álamo"
+    },
+    "36": {
+        "pol": "trzmielina",
+        "eng": "euonymus",
+        "ger": "Euonymus",
+        "esp": "Euonymus"
+    },
+    "37": {
+        "pol": "wiąz",
+        "eng": "elm",
+        "ger": "Ulme",
+        "esp": "olmo"
+    },
+    "38": {
+        "pol": "wierzba",
+        "eng": "willow",
+        "ger": "Weide",
+        "esp": "sauce"
+    },
+    "39": {
+        "pol": "wiśnia",
+        "eng": "cherry",
+        "ger": "Kirsche",
+        "esp": "cereza"
+    }
+}
+
+export { wordsTrees1, wordsTrees2 };
