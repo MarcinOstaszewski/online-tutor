a83c8be39fec928988a6c459887dc15f5d04b6f1
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-11 22:59:02 +0100

    add list Fruits+Nuts

---
+++ src/assets/wordLists/wordsFruitsNuts.js
@@ -0,0 +1,368 @@
+const wordsFruitsNuts1 = {
+     "1": {
+      "pol": "agrest",
+      "eng": "gooseberry",
+      "ger": "Stachelbeere",
+      "esp": "grosella"
+   },
+   "2": {
+      "pol": "ananas",
+      "eng": "pineapple",
+      "ger": "Ananas",
+      "esp": "piña"
+   },
+   "3": {
+      "pol": "arbuz",
+      "eng": "watermelon",
+      "ger": "Wassermelone",
+      "esp": "sandía"
+   },
+   "4": {
+      "pol": "aronia",
+      "eng": "chokeberry",
+      "ger": "Apfelbeere",
+      "esp": "cornijuelo"
+   },
+   "5": {
+      "pol": "awokado",
+      "eng": "avocado",
+      "ger": "Avocado",
+      "esp": "aguacate"
+   },
+   "6": {
+      "pol": "banan",
+      "eng": "banana",
+      "ger": "Banane",
+      "esp": "plátano"
+   },
+   "7": {
+      "pol": "biała porzeczka",
+      "eng": "white currant",
+      "ger": "weiße Johannisbeere",
+      "esp": "grosella blanca"
+   },
+   "8": {
+      "pol": "borówka amerykańska",
+      "eng": "blueberry",
+      "ger": "Blaubeere",
+      "esp": "arándano"
+   },
+   "9": {
+      "pol": "brzoskwinia",
+      "eng": "peach",
+      "ger": "Pfirsich",
+      "esp": "melocotón"
+   },
+   "10": {
+      "pol": "cytryna",
+      "eng": "lemon",
+      "ger": "Zitrone",
+      "esp": "limón"
+   },
+   "11": {
+      "pol": "czarna porzeczka",
+      "eng": "black currant",
+      "ger": "schwarze Johannisbeere",
+      "esp": "grosella negra"
+   },
+   "12": {
+      "pol": "czarny bez",
+      "eng": "black lilac",
+      "ger": "Flieder",
+      "esp": "lila negra"
+   },
+   "13": {
+      "pol": "czereśnia",
+      "eng": "cherry",
+      "ger": "Kirsche",
+      "esp": "cereza"
+   },
+   "14": {
+      "pol": "czerwona porzeczka",
+      "eng": "red currant",
+      "ger": "rote Johannisbeere",
+      "esp": "grosella roja"
+   },
+   "15": {
+      "pol": "daktyl",
+      "eng": "dactyl",
+      "ger": "Daktylus",
+      "esp": "dáctilo"
+   },
+   "16": {
+      "pol": "dzika róża",
+      "eng": "wild rose",
+      "ger": "wilde Rose",
+      "esp": "rosa salvaje"
+   },
+   "17": {
+      "pol": "figa",
+      "eng": "fig",
+      "ger": "Feige",
+      "esp": "higo"
+   },
+   "18": {
+      "pol": "granat",
+      "eng": "grenade",
+      "ger": "Granate",
+      "esp": "granada"
+   },
+   "19": {
+      "pol": "grejpfrut",
+      "eng": "grapefruit",
+      "ger": "Grapefruit",
+      "esp": "pomelo"
+   },
+   "20": {
+      "pol": "gruszka",
+      "eng": "pear",
+      "ger": "Birne",
+      "esp": "pera"
+   }
+}
+const wordsFruitsNuts2 = {
+    "21": {
+      "pol": "jabłko",
+      "eng": "apple",
+      "ger": "Apfel",
+      "esp": "manzana"
+   },
+   "22": {
+      "pol": "jarzębina",
+      "eng": "mountain ash",
+      "ger": "Eberesche",
+      "esp": "serbal"
+   },
+   "23": {
+      "pol": "jeżyna",
+      "eng": "blackberry",
+      "ger": "Brombeere",
+      "esp": "zarzamora"
+   },
+   "24": {
+      "pol": "kasztan jadalny",
+      "eng": "chestnut",
+      "ger": "Kastanie",
+      "esp": "castaño"
+   },
+   "25": {
+      "pol": "kiwi",
+      "eng": "kiwi",
+      "ger": "Kiwi",
+      "esp": "kiwi"
+   },
+   "26": {
+      "pol": "kokos",
+      "eng": "coconut",
+      "ger": "Kokosnuss",
+      "esp": "coco"
+   },
+   "27": {
+      "pol": "kumkwat",
+      "eng": "kumquat",
+      "ger": "Kumquat",
+      "esp": "naranja china"
+   },
+   "28": {
+      "pol": "liczi",
+      "eng": "lychee",
+      "ger": "Litschi",
+      "esp": "lychee"
+   },
+   "29": {
+      "pol": "limonka",
+      "eng": "lime",
+      "ger": "Limette",
+      "esp": "cal"
+   },
+   "30": {
+      "pol": "makadamia",
+      "eng": "macadamia",
+      "ger": "Macadamia",
+      "esp": "macadamia"
+   },
+   "31": {
+      "pol": "malina",
+      "eng": "raspberry",
+      "ger": "Himbeere",
+      "esp": "frambuesa"
+   },
+   "32": {
+      "pol": "mandarynka",
+      "eng": "mandarin",
+      "ger": "Mandarin",
+      "esp": "mandarina"
+   },
+   "33": {
+      "pol": "mango",
+      "eng": "mango",
+      "ger": "Mango",
+      "esp": "mango"
+   },
+   "34": {
+      "pol": "melon",
+      "eng": "melon",
+      "ger": "Melone",
+      "esp": "melón"
+   },
+   "35": {
+      "pol": "migdał",
+      "eng": "almond",
+      "ger": "Mandel",
+      "esp": "almendra"
+   },
+   "36": {
+      "pol": "morela",
+      "eng": "apricot",
+      "ger": "Aprikose",
+      "esp": "albaricoque"
+   },
+   "37": {
+      "pol": "morwa",
+      "eng": "mulberry",
+      "ger": "Maulbeere",
+      "esp": "morera"
+   },
+   "38": {
+      "pol": "nerkowiec",
+      "eng": "cashew",
+      "ger": "Cashew",
+      "esp": "anacardo"
+   },
+   "39": {
+      "pol": "nektarynka",
+      "eng": "nectarine",
+      "ger": "Nektarine",
+      "esp": "nectarina"
+   },
+   "40": {
+      "pol": "opuncja",
+      "eng": "prickly pear",
+      "ger": "Feigenkaktus",
+      "esp": "nopal"
+   }
+}
+const wordsFruitsNuts3 = {
+   "41": {
+      "pol": "orzech brazylijski",
+      "eng": "brazil nut",
+      "ger": "Paranuss",
+      "esp": "nuez de brasil"
+   },
+   "42": {
+      "pol": "orzech laskowy",
+      "eng": "hazelnut",
+      "ger": "Haselnuss",
+      "esp": "avellana"
+   },
+   "43": {
+      "pol": "orzech włoski",
+      "eng": "walnut",
+      "ger": "Walnuss",
+      "esp": "nuez"
+   },
+   "44": {
+      "pol": "orzech ziemny",
+      "eng": "peanut",
+      "ger": "Erdnuss",
+      "esp": "maní"
+   },
+   "45": {
+      "pol": "papaja",
+      "eng": "papaya",
+      "ger": "Papaya",
+      "esp": "papaya"
+   },
+   "46": {
+      "pol": "pekan",
+      "eng": "pecans",
+      "ger": "Pecannüsse",
+      "esp": "pacanas"
+   },
+   "47": {
+      "pol": "persymona",
+      "eng": "persimmon",
+      "ger": "Persimmon",
+      "esp": "caqui"
+   },
+   "48": {
+      "pol": "pigwa",
+      "eng": "quince",
+      "ger": "Quitte",
+      "esp": "membrillo"
+   },
+   "49": {
+      "pol": "orzeszki piniowe",
+      "eng": "pine nuts",
+      "ger": "Pinienkerne",
+      "esp": "piñones"
+   },
+   "50": {
+      "pol": "pistacja",
+      "eng": "pistachio",
+      "ger": "Pistazie",
+      "esp": "pistacho"
+   },
+   "51": {
+      "pol": "pomarańcza",
+      "eng": "orange",
+      "ger": "Orange",
+      "esp": "naranja"
+   },
+   "52": {
+      "pol": "poziomka",
+      "eng": "wild strawberry",
+      "ger": "wilde Erdbeere",
+      "esp": "fresa silvestre"
+   },
+   "53": {
+      "pol": "rambutan",
+      "eng": "rambutan",
+      "ger": "Rambutan",
+      "esp": "rambután"
+   },
+   "54": {
+      "pol": "śliwka",
+      "eng": "plum",
+      "ger": "Pflaume",
+      "esp": "ciruela"
+   },
+   "55": {
+      "pol": "tamarillo",
+      "eng": "tamarillo",
+      "ger": "Tamarillo",
+      "esp": "tomate de árbol"
+   },
+   "56": {
+      "pol": "tarnina",
+      "eng": "blackthorn",
+      "ger": "Schwarzdorn",
+      "esp": "endrino"
+   },
+   "57": {
+      "pol": "truskawka",
+      "eng": "strawberry",
+      "ger": "Erdbeere",
+      "esp": "fresa"
+   },
+   "58": {
+      "pol": "winogrono",
+      "eng": "grape",
+      "ger": "Traube",
+      "esp": "uva"
+   },
+   "59": {
+      "pol": "wiśnia",
+      "eng": "cherry",
+      "ger": "Kirsche",
+      "esp": "cereza"
+   },
+   "60": {
+      "pol": "żurawina",
+      "eng": "cranberry",
+      "ger": "Preiselbeere",
+      "esp": "arándano agrio"
+   }
+}
+
+export {wordsFruitsNuts1, wordsFruitsNuts2, wordsFruitsNuts3 }
