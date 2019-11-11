146cea3ce9d8b04e0f7dc818a943797afab985dd
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-11 23:04:56 +0100

    modified animals list to 4 languages

--- src/assets/wordLists/wordsAnimals.js
+++ src/assets/wordLists/wordsAnimals.js
@@ -1,231 +1,349 @@
 const wordsAnimals1 = {
-    0: {
-        pol: 'jeż', 
-        eng: 'hedgehog'
-    },
-    1: {
-        pol: 'lis', 
-        eng: 'fox'
-    },
-    2: {
-        pol: 'lew', 
-        eng: 'lion'
-    },
-    3: {
-        pol: 'pantera',
-        eng: 'panther'
-    },
-    4: {
-        pol: 'żyrafa', 
-        eng: 'giraffe'
-    },
-    5: {
-        pol: 'słoń', 
-        eng: 'elephant'
-    },
-    6: {
-        pol: 'wielbłąd',
-        eng: 'camel'
-    },
-    7: {
-        pol: 'pawian',
-        eng: 'baboon'
-    },
-    8: {
-        pol: 'goryl',
-        eng: 'gorilla'
-    },
-    9: {
-        pol: 'nosorożec',
-        eng: 'rhinoceros'
-    },
-    10: {
-        pol: 'kurczak',
-        eng: 'chicken'
-    },
-    11: {
-        pol: 'kura',
-        eng: 'hen'
-    },
-    12: {
-        pol: 'gibon',
-        eng: 'gibbon'
-    },
-    13: {
-        pol: 'królik', 
-        eng: 'rabbit'
-    },
-    14: {
-        pol: 'ryba', 
-        eng: 'fish'
-    },
-    15: {
-        pol: 'niedźwiedź', 
-        eng: 'bear'
-    },
-    16: {
-        pol: 'krokodyl',
-        eng: 'crockodile'
-    },
-    17: {
-        pol: 'małpa',
-        eng: 'monkey'
-    },
-    18: {
-        pol: 'ptak',
-        eng: 'bird'
-    },
-    19: {
-        pol: 'jeleń',
-        eng: 'deer'
-    },
-    20: {
-        pol: 'mysz',
-        eng: 'mouse'
-    }
+     "1": {
+      "pol": "bawół",
+      "eng": "buffalo",
+      "ger": "Büffel",
+      "esp": "búfalo"
+   },
+   "2": {
+      "pol": "chomik",
+      "eng": "hamster",
+      "ger": "Hamster",
+      "esp": "hámster"
+   },
+   "3": {
+      "pol": "daniel",
+      "eng": "fallow deer",
+      "ger": "Damhirsch",
+      "esp": "gamo"
+   },
+   "4": {
+      "pol": "dzik",
+      "eng": "boar",
+      "ger": "Eber",
+      "esp": "verraco"
+   },
+   "5": {
+      "pol": "dziobak",
+      "eng": "platypus",
+      "ger": "Schnabeltier",
+      "esp": "ornitorrinco"
+   },
+   "6": {
+      "pol": "gibon",
+      "eng": "gibbon",
+      "ger": "Gibbon",
+      "esp": "gibón"
+   },
+   "7": {
+      "pol": "gołąb",
+      "eng": "pidgeon",
+      "ger": "Pidgeon",
+      "esp": "paloma"
+   },
+   "8": {
+      "pol": "goryl",
+      "eng": "gorilla",
+      "ger": "Gorilla",
+      "esp": "gorila"
+   },
+   "9": {
+      "pol": "jagnię",
+      "eng": "lamb",
+      "ger": "Lamm",
+      "esp": "cordero"
+   },
+   "10": {
+      "pol": "jastrząb",
+      "eng": "hawk",
+      "ger": "Falke",
+      "esp": "halcón"
+   },
+   "11": {
+      "pol": "jaszczurka",
+      "eng": "lizzard",
+      "ger": "Eidechse",
+      "esp": "lagarto"
+   },
+   "12": {
+      "pol": "jeleń",
+      "eng": "deer",
+      "ger": "Hirsch",
+      "esp": "ciervo"
+   },
+   "13": {
+      "pol": "jeż",
+      "eng": "hedgehog",
+      "ger": "Igel",
+      "esp": "erizo"
+   },
+   "14": {
+      "pol": "kaczka",
+      "eng": "duck",
+      "ger": "Ente",
+      "esp": "pato"
+   },
+   "15": {
+      "pol": "kangur",
+      "eng": "kangaroo",
+      "ger": "Känguru",
+      "esp": "canguro"
+   },
+   "16": {
+      "pol": "koala",
+      "eng": "coala",
+      "ger": "Koala",
+      "esp": "koala"
+   },
+   "17": {
+      "pol": "koń",
+      "eng": "horse",
+      "ger": "Pferd",
+      "esp": "caballo"
+   },
+   "18": {
+      "pol": "koza",
+      "eng": "goat",
+      "ger": "Ziege",
+      "esp": "cabra"
+   },
+   "19": {
+      "pol": "kret",
+      "eng": "mole",
+      "ger": "Maulwurf ",
+      "esp": "topo"
+   },
+   "20": {
+      "pol": "krokodyl",
+      "eng": "crockodile",
+      "ger": "Krokodil",
+      "esp": "cocodrilo"
+   }
 }
 const wordsAnimals2 = {
-    21: {
-        pol: 'żaba',
-        eng: 'frog'
+    "21": {
+        "pol": "królik",
+        "eng": "rabbit",
+        "ger": "Kaninchen",
+        "esp": "conejo"
     },
-    22: {
-        pol: 'koza',
-        eng: 'goat'
+    "22": {
+        "pol": "kucyk",
+        "eng": "pony",
+        "ger": "Pony",
+        "esp": "poni"
     },
-    23: {
-        pol: 'chomik', 
-        eng: 'hamster'
+    "23": {
+        "pol": "kuna",
+        "eng": "marten",
+        "ger": "Marder",
+        "esp": "marta"
     },
-    24: {
-        pol: 'koń', 
-        eng: 'horse'
+    "24": {
+        "pol": "kura",
+        "eng": "hen",
+        "ger": "Henne",
+        "esp": "gallina"
     },
-    25: {
-        pol: 'kangur', 
-        eng: 'kangaroo'
+    "25": {
+        "pol": "kurczak",
+        "eng": "chicken",
+        "ger": "Hähnchen",
+        "esp": "pollo"
     },
-    26: {
-        pol: 'owca',
-        eng: 'sheep'
+    "26": {
+        "pol": "lew",
+        "eng": "lion",
+        "ger": "Löwe",
+        "esp": "león"
     },
-    27: {
-        pol: 'papuga',
-        eng: 'parrot'
+    "27": {
+        "pol": "lis",
+        "eng": "fox",
+        "ger": "Fuchs",
+        "esp": "zorro"
     },
-    28: {
-        pol: 'świnia',
-        eng: 'pig'
+    "28": {
+        "pol": "łoś",
+        "eng": "moose",
+        "ger": "Elch",
+        "esp": "alce"
     },
-    29: {
-        pol: 'kucyk',
-        eng: 'pony'
+    "29": {
+        "pol": "małpa",
+        "eng": "monkey",
+        "ger": "Affe",
+        "esp": "mono"
     },
-    30: {
-        pol: 'szczur',
-        eng: 'rat'
+    "30": {
+        "pol": "mysz",
+        "eng": "mouse",
+        "ger": "Maus",
+        "esp": "ratón"
     },
-    31: {
-        pol: 'wiewiórka',
-        eng: 'squirrel'
+    "31": {
+        "pol": "niedźwiedź",
+        "eng": "bear",
+        "ger": "Bär",
+        "esp": "oso"
     },
-    32: {
-        pol: 'ropucha',
-        eng: 'toad'
+    "32": {
+        "pol": "niedźwiedź polarny",
+        "eng": "polar bear",
+        "ger": "Eisbär",
+        "esp": "oso polar"
     },
-    33: {
-        pol: 'tygrys', 
-        eng: 'tiger'
+    "33": {
+        "pol": "nosorożec",
+        "eng": "rhinoceros",
+        "ger": "Nashorn",
+        "esp": "rinoceronte"
     },
-    34: {
-        pol: 'wieloryb', 
-        eng: 'whale'
+    "34": {
+        "pol": "orzeł",
+        "eng": "eagle",
+        "ger": "Adler",
+        "esp": "águila"
     },
-    35: {
-        pol: 'wilk', 
-        eng: 'wolf'
+    "35": {
+        "pol": "owca",
+        "eng": "sheep",
+        "ger": "Schaf",
+        "esp": "oveja"
     },
-    36: {
-        pol: 'jagnię', 
-        eng: 'lamb'
+    "36": {
+        "pol": "pantera",
+        "eng": "panther",
+        "ger": "Panther",
+        "esp": "pantera"
     },
-    37: {
-        pol: 'wąż', 
-        eng: 'snake'
+    "37": {
+        "pol": "papuga",
+        "eng": "parrot",
+        "ger": "Papagei",
+        "esp": "loro"
     },
-    38: {
-        pol: 'jaszczurka',
-        eng: 'lizzard'
+    "38": {
+        "pol": "pawian",
+        "eng": "baboon",
+        "ger": "Pavian",
+        "esp": "babuino"
     },
-    39: {
-        pol: 'orzeł',
-        eng: 'eagle'
+    "39": {
+        "pol": "ptak",
+        "eng": "bird",
+        "ger": "Vogel",
+        "esp": "pájaro"
     },
-    40: {
-        pol: 'jastrząb',
-        eng: 'hawk'
+    "40": {
+        "pol": "ropucha",
+        "eng": "toad",
+        "ger": "Kröte",
+        "esp": "sapo"
     }
 }
 const wordsAnimals3 = {
-    41: {
-        pol: 'kaczka',
-        eng: 'duck'
+    "41": {
+        "pol": "ryba",
+        "eng": "fish",
+        "ger": "Fisch",
+        "esp": "pescado"
     },
-    42: {
-        pol: 'bawół',
-        eng: 'buffalo'
+    "42": {
+        "pol": "ryś",
+        "eng": "Lynx",
+        "ger": "Luchs",
+        "esp": "lince"
     },
-    43: {
-        pol: 'niedźwiedź polarny', 
-        eng: 'polar bear'
+    "43": {
+        "pol": "sarna",
+        "eng": "roe",
+        "ger": "Rogen",
+        "esp": "hueva"
     },
-    44: {
-        pol: 'koala', 
-        eng: 'coala'
+    "44": {
+        "pol": "słoń",
+        "eng": "elephant",
+        "ger": "Elefant",
+        "esp": "elefante"
     },
-    45: {
-        pol: 'dziobak', 
-        eng: 'platypus'
+    "45": {
+        "pol": "szczur",
+        "eng": "rat",
+        "ger": "Ratte",
+        "esp": "rata"
     },
-    46: {
-        pol: 'wróbel', 
-        eng: 'sparow'
+    "46": {
+        "pol": "świnia",
+        "eng": "pig",
+        "ger": "Schwein",
+        "esp": "cerdo"
     },
-    47: {
-        pol: 'gołąb', 
-        eng: 'pidgeon'
+    "47": {
+        "pol": "tygrys",
+        "eng": "tiger",
+        "ger": "Tiger",
+        "esp": "Tigre"
     },
-    48: {
-        pol: 'daniel',
-        eng: 'fallow deer'
+    "48": {
+        "pol": "wąż",
+        "eng": "snake",
+        "ger": "Schlange",
+        "esp": "serpiente"
     },
-    49: {
-        pol: 'dzik',
-        eng: 'boar'
+    "49": {
+        "pol": "wielbłąd",
+        "eng": "camel",
+        "ger": "Kamel",
+        "esp": "camello"
     },
-    50: {
-        pol: 'kuna',
-        eng: 'marten'
+    "50": {
+        "pol": "wieloryb",
+        "eng": "whale",
+        "ger": "Wal",
+        "esp": "ballena"
     },
-    51: {
-        pol: 'łoś',
-        eng: 'moose'
+    "51": {
+        "pol": "wiewiórka",
+        "eng": "squirrel",
+        "ger": "Eichhörnchen",
+        "esp": "ardilla"
     },
-    52: {
-        pol: 'ryś',
-        eng: 'Lynx'
+    "52": {
+        "pol": "wilk",
+        "eng": "wolf",
+        "ger": "Wolf",
+        "esp": "lobo"
     },
-    53: {
-        pol: 'sarna',
-        eng: 'roe'
+    "53": {
+        "pol": "wróbel",
+        "eng": "sparow",
+        "ger": "Spatz",
+        "esp": "gorrión"
     },
-    54: {
-        pol: 'zając',
-        eng: 'hare'
+    "54": {
+        "pol": "zając",
+        "eng": "hare",
+        "ger": "Hase",
+        "esp": "liebre"
     },
-    55: {
-        pol: 'żubr',
-        eng: 'aurochs'
+    "55": {
+        "pol": "żaba",
+        "eng": "frog",
+        "ger": "Frosch",
+        "esp": "rana"
+    },
+    "56": {
+        "pol": "żubr",
+        "eng": "aurochs",
+        "ger": "Auerochsen",
+        "esp": "uro"
+    },
+    "57": {
+        "pol": "żyrafa",
+        "eng": "giraffe",
+        "ger": "Giraffe",
+        "esp": "jirafa"
     }
 }
 
