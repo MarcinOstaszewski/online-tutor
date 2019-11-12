630495ac5e2774e61113233ddac8a7f19287e78c
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-13 00:16:02 +0100

    moved MainMenu to Main

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, {Component} from 'react';
 import Main from './components/Main/Main'
-import MainMenu from './containers/MainMenu/MainMenu'
-import styles from './App.module.css';
+
+// import './App.module.css';
 
 class App extends Component {
-	state = {
-		chosenListName: ''
-	}
-	
-	wordListChosen = (e) => {
-		this.setState({
-			chosenListName: 'words' + e.currentTarget.innerText.replace(' ','')
-		})
-	}
 
 	render(){
 
 		return (
 			<div className="App">
-				<header>
-					<MainMenu wordListClicked={this.wordListChosen}/>
-					<div className={styles.NavBar}>NAVBAR</div>
-				</header>
 				
-				<Main chosenListName={this.state.chosenListName}/>
+				<Main />
 
 				<footer>Online-Tutor by MarcinO</footer>
 
