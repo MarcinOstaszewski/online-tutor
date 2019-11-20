66c3bd6d1bfe3d9b5eee1a6e9489104baf5f342f
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-20 22:03:00 +0100

    add language names in Selector

--- src/containers/LanguagesSelector/LanguagesSelector.js
+++ src/containers/LanguagesSelector/LanguagesSelector.js
@@ -5,21 +5,30 @@ import styles from './LanguagesSelector.module.css';
 
 const LanguagesSelector = (props) => {
     
+    // console.log(props.isActive)
+
+
     return ( 
-        <div className={styles.Centered}>
-            <LanguageSelect 
-                languageClicked={props.langChosenHandler} 
-                languages={props.languages}
-                isActive={props.isActive}
-                role="q" 
-                />
-            <div className={styles.Divider}> => </div>
-            <LanguageSelect  
-                languageClicked={props.langChosenHandler} 
-                languages={props.languages}
-                isActive={props.isActive}
-                role="a" 
-                />
+        <div >
+            <div className={styles.LangInfo}>
+                <div>Język ojczysty:</div>
+                <div>Foreign language:</div>
+            </div>
+            <div className={styles.Selector}>
+                <LanguageSelect 
+                    languageClicked={props.langChosenHandler} 
+                    languages={props.languages}
+                    isActive={props.isActive}
+                    role="q" 
+                    />
+                <div className={styles.Divider}> >>> </div>
+                <LanguageSelect  
+                    languageClicked={props.langChosenHandler} 
+                    languages={props.languages}
+                    isActive={props.isActive}
+                    role="a" 
+                    />
+            </div>
         </div>
      );
 }
