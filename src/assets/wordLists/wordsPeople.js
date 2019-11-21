d696d95a9b51ff0873def9d4471c6780aa14c2ae
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-21 22:50:43 +0100

    add People word list

---
+++ src/assets/wordLists/wordsPeople.js
@@ -0,0 +1,118 @@
+const wordsPeople = {
+    "1": {
+        "pol": "dziecko",
+        "eng": "child",
+        "ger": "Kind",
+        "esp": "niño"
+    },
+    "2": {
+        "pol": "dorosły",
+        "eng": "adult",
+        "ger": "Erwachsene",
+        "esp": "adulto"
+    },
+    "3": {
+        "pol": "młodzieniec",
+        "eng": "lad",
+        "ger": "Junge",
+        "esp": "muchacho"
+    },
+    "4": {
+        "pol": "dziewczyna",
+        "eng": "girl",
+        "ger": "Mädchen",
+        "esp": "chica"
+    },
+    "5": {
+        "pol": "chłopak",
+        "eng": "boy",
+        "ger": "Junge",
+        "esp": "chico"
+    },
+    "6": {
+        "pol": "brat",
+        "eng": "brother",
+        "ger": "Bruder",
+        "esp": "hermano"
+    },
+    "7": {
+        "pol": "siostra",
+        "eng": "sister",
+        "ger": "Schwester",
+        "esp": "hermana"
+    },
+    "8": {
+        "pol": "mama ",
+        "eng": "mom",
+        "ger": "Mutter",
+        "esp": "madre"
+    },
+    "9": {
+        "pol": "tata ",
+        "eng": "dad",
+        "ger": "Papa",
+        "esp": "papá"
+    },
+    "10": {
+        "pol": "dziadek",
+        "eng": "grandfather",
+        "ger": "Großvater",
+        "esp": "abuelo"
+    },
+    "11": {
+        "pol": "babcia",
+        "eng": "grandmother",
+        "ger": "Großmutter",
+        "esp": "abuela"
+    },
+    "12": {
+        "pol": "ciotka",
+        "eng": "aunt",
+        "ger": "Tante",
+        "esp": "tía"
+    },
+    "13": {
+        "pol": "wuj",
+        "eng": "uncle",
+        "ger": "Onkel",
+        "esp": "tío"
+    },
+    "15": {
+        "pol": "kuzyn",
+        "eng": "cousin",
+        "ger": "Vetter",
+        "esp": "primo"
+    },
+    "16": {
+        "pol": "sąsiad",
+        "eng": "neighbor",
+        "ger": "Nachbar",
+        "esp": "vecino"
+    },
+    "17": {
+        "pol": "przyjaciel",
+        "eng": "friend",
+        "ger": "Freund",
+        "esp": "amigo"
+    },
+    "18": {
+        "pol": "znajomy",
+        "eng": "acquaintance",
+        "ger": "Bekannte",
+        "esp": "conocido"
+    },
+    "19": {
+        "pol": "kolega",
+        "eng": "colleague",
+        "ger": "Kollege",
+        "esp": "colega"
+    },
+    "20": {
+        "pol": "kumpel",
+        "eng": "buddy",
+        "ger": "Kumpel",
+        "esp": "camarada"
+    }
+}
+
+export { wordsPeople };
