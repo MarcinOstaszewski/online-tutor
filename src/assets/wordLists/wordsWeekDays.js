33c701eec9017f5443a8ef8c91365e0883927799
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-23 14:14:15 +0100

    add WeekDays word list

---
+++ src/assets/wordLists/wordsWeekDays.js
@@ -0,0 +1,46 @@
+const wordsWeekDays = {
+    "1": {
+        "pol": "poniedziałek",
+        "eng": "Monday",
+        "ger": "Montag",
+        "esp": "lunes"
+    },
+    "2": {
+        "pol": "wtorek",
+        "eng": "Tuesday",
+        "ger": "Dienstag",
+        "esp": "martes"
+    },
+    "3": {
+        "pol": "środa",
+        "eng": "Wednesday",
+        "ger": "Mittwoch",
+        "esp": "miércoles"
+    },
+    "4": {
+        "pol": "czwartek",
+        "eng": "Thursday",
+        "ger": "Donnerstag",
+        "esp": "jueves"
+    },
+    "5": {
+        "pol": "piątek",
+        "eng": "Friday",
+        "ger": "Freitag",
+        "esp": "viernes"
+    },
+    "6": {
+        "pol": "sobota",
+        "eng": "Saturday",
+        "ger": "Samstag",
+        "esp": "sábado"
+    },
+    "7": {
+        "pol": "niedziela",
+        "eng": "Sunday",
+        "ger": "Sonntag",
+        "esp": "domingo"
+    }
+}
+
+export { wordsWeekDays };
