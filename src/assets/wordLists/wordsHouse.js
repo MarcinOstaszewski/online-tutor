b9c41a8fb93e2cba9e51b656c349a3452ea421a2
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-23 23:10:55 +0100

    add few words list - House

---
+++ src/assets/wordLists/wordsHouse.js
@@ -0,0 +1,522 @@
+const wordsBathroom = {
+    "0": {
+        "pol": "łazienka",
+        "eng": "bathroom",
+        "ger": "Bad",
+        "esp": "cuarto de baño"
+    },
+    "1": {
+        "pol": "szampon",
+        "eng": "shampoo",
+        "ger": "Shampoo",
+        "esp": "champú"
+    },
+    "2": {
+        "pol": "ręcznik",
+        "eng": "towel",
+        "ger": "Handtuch",
+        "esp": "toalla"
+    },
+    "3": {
+        "pol": "mydło",
+        "eng": "soap",
+        "ger": "Seife",
+        "esp": "jabón"
+    },
+    "4": {
+        "pol": "szczoteczka do zębów",
+        "eng": "toothbrush",
+        "ger": "Zahnbürste",
+        "esp": "cepillo de dientes"
+    },
+    "5": {
+        "pol": "prysznic",
+        "eng": "shower",
+        "ger": "Dusche",
+        "esp": "ducha"
+    },
+    "6": {
+        "pol": "pralka",
+        "eng": "washing machine",
+        "ger": "Waschmaschine",
+        "esp": "almohadilla"
+    },
+    "7": {
+        "pol": "kran",
+        "eng": "tap",
+        "ger": "Zapfhahn",
+        "esp": "grifo"
+    },
+    "8": {
+        "pol": "papier toaletowy",
+        "eng": "toilet paper",
+        "ger": "Toilettenpapier",
+        "esp": "papel higienico"
+    },
+    "9": {
+        "pol": "toaleta",
+        "eng": "toilet",
+        "ger": "Toilette",
+        "esp": "WC"
+    },
+    "10": {
+        "pol": "kąpiel",
+        "eng": "bath",
+        "ger": "Bad",
+        "esp": "baño"
+    },
+    "11": {
+        "pol": "pasta do zębów",
+        "eng": "toothpaste",
+        "ger": "Zahnpasta",
+        "esp": "pasta de dientes"
+    },
+    "12": {
+        "pol": "gabinet",
+        "eng": "cabinet",
+        "ger": "Kabinett",
+        "esp": "oficina"
+    },
+    "13": {
+        "pol": "szczotka do włosów",
+        "eng": "hairbrush",
+        "ger": "Haarbürste",
+        "esp": "cepillo para el cabello"
+    },
+    "14": {
+        "pol": "wanna",
+        "eng": "bathtub",
+        "ger": "Badewanne",
+        "esp": "baño"
+    },
+    "15": {
+        "pol": "grzebień",
+        "eng": "comb",
+        "ger": "Kamm",
+        "esp": "peine"
+    },
+    "16": {
+        "pol": "lustro",
+        "eng": "mirror",
+        "ger": "Spiegel",
+        "esp": "espejo"
+    },
+    "17": {
+        "pol": "perfumy",
+        "eng": "perfume",
+        "ger": "Parfüm",
+        "esp": "perfume"
+    },
+    "18": {
+        "pol": "szminka",
+        "eng": "lipstick",
+        "ger": "Lippenstift",
+        "esp": "lápiz labial"
+    },
+    "19": {
+        "pol": "szlafrok",
+        "eng": "bathrobe",
+        "ger": "Bademantel",
+        "esp": "bata"
+    },
+    "20": {
+        "pol": "umywalka",
+        "eng": "sink",
+        "ger": "Waschbecken",
+        "esp": "lavabo"
+    }
+}
+const wordsHome = {
+    "21": {
+        "pol": "pokój",
+        "eng": "room",
+        "ger": "Zimmer",
+        "esp": "habitación"
+    },
+    "22": {
+        "pol": "obraz",
+        "eng": "image",
+        "ger": "Bild",
+        "esp": "imagen"
+    },
+    "23": {
+        "pol": "stół",
+        "eng": "table",
+        "ger": "Tabelle",
+        "esp": "mesa"
+    },
+    "24": {
+        "pol": "szafka",
+        "eng": "cupboard",
+        "ger": "Schrank",
+        "esp": "armario"
+    },
+    "25": {
+        "pol": "fotel",
+        "eng": "armchair",
+        "ger": "Stuhl",
+        "esp": "silla"
+    },
+    "26": {
+        "pol": "krzesło",
+        "eng": "chair",
+        "ger": "Stuhl",
+        "esp": "silla"
+    },
+    "27": {
+        "pol": "taboret",
+        "eng": "stool",
+        "ger": "Hocker",
+        "esp": "taburete"
+    },
+    "28": {
+        "pol": "zegar",
+        "eng": "clock",
+        "ger": "Uhr",
+        "esp": "reloj"
+    },
+    "29": {
+        "pol": "telewizor",
+        "eng": "television set",
+        "ger": "Fernsehen",
+        "esp": "televisión"
+    },
+    "30": {
+        "pol": "półka",
+        "eng": "the shelf",
+        "ger": "Regal",
+        "esp": "estante"
+    },
+    "31": {
+        "pol": "komputer",
+        "eng": "computer",
+        "ger": "Computer",
+        "esp": "ordenador"
+    },
+    "32": {
+        "pol": "telefon",
+        "eng": "phone",
+        "ger": "Telefon",
+        "esp": "teléfono"
+    }
+}
+const wordsKitchen = {
+    "33": {
+        "pol": "kuchnia",
+        "eng": "kitchen",
+        "ger": "Küche",
+        "esp": "cocina"
+    },
+    "34": {
+        "pol": "lodówka",
+        "eng": "fridge",
+        "ger": "Kühlschrank",
+        "esp": "nevera"
+    },
+    "35": {
+        "pol": "kuchenka",
+        "eng": "stove",
+        "ger": "Kocher",
+        "esp": "estufa"
+    },
+    "36": {
+        "pol": "blat ",
+        "eng": "countertop",
+        "ger": "Arbeitsplatte ",
+        "esp": "encimera"
+    },
+    "37": {
+        "pol": "zlew",
+        "eng": "sink",
+        "ger": "Spüle",
+        "esp": "fregadero"
+    },
+    "38": {
+        "pol": "nóż",
+        "eng": "knife",
+        "ger": "Messer",
+        "esp": "cuchillo"
+    },
+    "39": {
+        "pol": "widelec",
+        "eng": "fork",
+        "ger": "Gabel",
+        "esp": "tenedor"
+    },
+    "40": {
+        "pol": "łyżka",
+        "eng": "spoon",
+        "ger": "Löffel",
+        "esp": "cuchara"
+    },
+    "41": {
+        "pol": "talerz",
+        "eng": "a plate",
+        "ger": "Platte",
+        "esp": "placa"
+    },
+    "42": {
+        "pol": "kubek",
+        "eng": "mug",
+        "ger": "Becher",
+        "esp": "jarra"
+    },
+    "43": {
+        "pol": "szklanka",
+        "eng": "glass",
+        "ger": "Glas",
+        "esp": "vidrio"
+    },
+    "44": {
+        "pol": "spodek",
+        "eng": "saucer",
+        "ger": "Untertasse ",
+        "esp": "platillo"
+    },
+    "45": {
+        "pol": "filiżanka",
+        "eng": "cup",
+        "ger": "Tasse",
+        "esp": "taza"
+    },
+    "46": {
+        "pol": "deska",
+        "eng": "board",
+        "ger": "Brett",
+        "esp": "tablero"
+    },
+    "47": {
+        "pol": "mikser",
+        "eng": "mixer",
+        "ger": "Mischer",
+        "esp": "mezclador"
+    },
+    "48": {
+        "pol": "garnek",
+        "eng": "pot",
+        "ger": "Topf",
+        "esp": "olla"
+    },
+    "49": {
+        "pol": "patelnia",
+        "eng": "pan",
+        "ger": "Bratpfanne",
+        "esp": "sartén"
+    },
+    "50": {
+        "pol": "przykrywka",
+        "eng": "lid",
+        "ger": "Deckel",
+        "esp": "tapa"
+    },
+    "67": {
+        "pol": "piekarnik",
+        "eng": "oven",
+        "ger": "Backofen",
+        "esp": "horno"
+    }
+}
+const wordsHousehold = {
+    "51": {
+        "pol": "sypialnia",
+        "eng": "bedroom",
+        "ger": "Schlafzimmer",
+        "esp": "dormitorio"
+    },
+    "52": {
+        "pol": "budzik",
+        "eng": "alarm clock",
+        "ger": "Wecker",
+        "esp": "despertador"
+    },
+    "53": {
+        "pol": "łóżko",
+        "eng": "bed",
+        "ger": "Bett",
+        "esp": "cama"
+    },
+    "54": {
+        "pol": "poduszka",
+        "eng": "cushion",
+        "ger": "Kissen",
+        "esp": "almohada"
+    },
+    "55": {
+        "pol": "prześcieradło",
+        "eng": "sheet",
+        "ger": "Blatt",
+        "esp": "hoja"
+    },
+    "56": {
+        "pol": "okno ",
+        "eng": "window",
+        "ger": "Fenster",
+        "esp": "ventana"
+    },
+    "57": {
+        "pol": "zasłona",
+        "eng": "curtain",
+        "ger": "Schleier",
+        "esp": "velo"
+    },
+    "58": {
+        "pol": "firanka ",
+        "eng": "curtain",
+        "ger": "Vorhang",
+        "esp": "cortina"
+    },
+    "59": {
+        "pol": "drzwi",
+        "eng": "door",
+        "ger": "Tür",
+        "esp": "puerta"
+    },
+    "60": {
+        "pol": "ściana",
+        "eng": "wall",
+        "ger": "Wand",
+        "esp": "pared"
+    },
+    "61": {
+        "pol": "przedpokój",
+        "eng": "hall",
+        "ger": "Vorraum",
+        "esp": "antesala"
+    },
+    "62": {
+        "pol": "strych",
+        "eng": "attic",
+        "ger": "Dachboden ",
+        "esp": "ático"
+    },
+    "63": {
+        "pol": "piwnica",
+        "eng": "cellar",
+        "ger": "Keller",
+        "esp": "bodega"
+    },
+    "64": {
+        "pol": "podłoga",
+        "eng": "floor",
+        "ger": "Boden",
+        "esp": "piso"
+    },
+    "65": {
+        "pol": "sufit",
+        "eng": "ceiling",
+        "ger": "Decke",
+        "esp": "techo"
+    },
+    "66": {
+        "pol": "parapet",
+        "eng": "windowsill",
+        "ger": "Fensterbank ",
+        "esp": "antepecho"
+    },
+    "68": {
+        "pol": "schody",
+        "eng": "stairs",
+        "ger": "Treppe",
+        "esp": "escalera"
+    },
+    "69": {
+        "pol": "poręcz",
+        "eng": "handrail",
+        "ger": "Geländer",
+        "esp": "barandilla"
+    },
+    "70": {
+        "pol": "balustrada",
+        "eng": "balustrade",
+        "ger": "Balustrade",
+        "esp": "balaustrada"
+    },
+    "71": {
+        "pol": "balkon",
+        "eng": "balcony",
+        "ger": "Balkon",
+        "esp": "balcón"
+    },
+    "72": {
+        "pol": "dach",
+        "eng": "roof",
+        "ger": "Dach",
+        "esp": "tejado"
+    },
+    "73": {
+        "pol": "kominek",
+        "eng": "fireplace",
+        "ger": "Kamin",
+        "esp": "hogar"
+    },
+    "74": {
+        "pol": "dywan",
+        "eng": "carpet",
+        "ger": "Teppich",
+        "esp": "alfombra"
+    },
+    "75": {
+        "pol": "spiżarnia",
+        "eng": "pantry",
+        "ger": "Speisekammer ",
+        "esp": "despensa"
+    },
+    "76": {
+        "pol": "książka",
+        "eng": "book",
+        "ger": "Buch",
+        "esp": "libro"
+    }
+}
+const wordsRelations = {
+    "77": {
+        "pol": "w",
+        "eng": "in",
+        "ger": "in",
+        "esp": "en"
+    },
+    "78": {
+        "pol": "na",
+        "eng": "on",
+        "ger": "es",
+        "esp": "lo"
+    },
+    "79": {
+        "pol": "pod",
+        "eng": "under",
+        "ger": "unter",
+        "esp": "bajo"
+    },
+    "80": {
+        "pol": "za",
+        "eng": "behind",
+        "ger": "hinter",
+        "esp": "detrás"
+    },
+    "81": {
+        "pol": "przed",
+        "eng": "in front of",
+        "ger": "vor",
+        "esp": "en frente de"
+    },
+    "82": {
+        "pol": "ponad",
+        "eng": "over",
+        "ger": "über",
+        "esp": "encima"
+    },
+    "83": {
+        "pol": "poniżej",
+        "eng": "below",
+        "ger": "unten",
+        "esp": "por debajo"
+    },
+    "84": {
+        "pol": "obok",
+        "eng": "next to",
+        "ger": "neben",
+        "esp": "al lado de"
+    }
+}
+
+export {wordsBathroom, wordsHome, wordsHousehold, wordsKitchen, wordsRelations };
