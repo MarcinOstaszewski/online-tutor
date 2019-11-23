ad200e2a2632e25371f3ffe129a61927c193b2d4
MarcinOstaszewski <marcinostaszewski@gmail.com>
2019-11-23 14:47:31 +0100

    add files tu run build

---
+++ build/precache-manifest.80bee8fd873401fa01dc37cfe86e109d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dff90969f49feb2ad60182cac2d8f18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a576b4dd710315b1563c",
+    "url": "/static/css/main.8251d4e8.chunk.css"
+  },
+  {
+    "revision": "917c6345dc5178542c1d",
+    "url": "/static/js/2.11e35f73.chunk.js"
+  },
+  {
+    "revision": "a576b4dd710315b1563c",
+    "url": "/static/js/main.36e33b97.chunk.js"
+  },
+  {
+    "revision": "5bf18dec633cde8d962f",
+    "url": "/static/js/runtime-main.81f0f287.js"
+  }
+]);
