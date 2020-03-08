c66f35bd980858fcd4b8bb53b60ab4d25fcdd9cd
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-03-08 21:58:25 +0100

    change display of Language Selector

--- src/containers/LanguagesSelector/LanguagesSelector.js
+++ src/containers/LanguagesSelector/LanguagesSelector.js
@@ -5,29 +5,25 @@ import styles from './LanguagesSelector.module.css';
 
 const LanguagesSelector = (props) => {
     
-    // console.log(props.isActive)
-
-
     return ( 
-        <div >
-            <div className={styles.LangInfo}>
-                <div>Język ojczysty:</div>
-                <div>Foreign language:</div>
-            </div>
-            <div className={styles.Selector}>
+        <div className={styles.Selector}>
+            <div className={styles.from}>
+                <p>From language:</p>
                 <LanguageSelect 
                     languageClicked={props.langChosenHandler} 
                     languages={props.languages}
                     isActive={props.isActive}
                     role="q" 
-                    />
-                <div className={styles.Divider}> >>> </div>
+                />
+            </div>
+            <div className={styles.to}>
+                <p>Translation to:</p>
                 <LanguageSelect  
                     languageClicked={props.langChosenHandler} 
                     languages={props.languages}
                     isActive={props.isActive}
                     role="a" 
-                    />
+                />
             </div>
         </div>
      );
